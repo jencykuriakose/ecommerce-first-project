ffd05dadb2461b6729a9d467739a5109ed8855fd
jencykuriakose <jjencykuriakose@gmail.com>
2023-11-17 14:00:51 +0530

    basic project setuped

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", (req, res) => {
+	res.render("home");
+});
+
+app.listen(PORT, () => {
+	console.log(`server is started ${PORT}`);
+});
